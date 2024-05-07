614e4fe4187499393c7dee589a1e8743d415646e
Firepup Sixfifty <firepyp650@gmail.com>
2024-05-07 13:00:24 -0500

    Correct first info for ssh script

--- fsss-ssh.sh
+++ fsss-ssh.sh
@@ -1,4 +1,4 @@
-echo '[INFO] Make sure to run this script as the user that forejo runs as!'
+echo '[INFO] Make sure to run this script as the user that forejo runs as, over ssh, or with manual ownership grants to your current tty!'
 echo '[INFO] Now creating a gpg key, please fill out the inforemation with what you want for forejo to use.'
 echo '$ gpg --default-new-key-algo rsa4096 --gen-key'
 gpg --default-new-key-algo rsa4096 --gen-key
