2dc58d9b7c8f025702f5d5f5d16d3d6d00350409
Firepup Sixfifty <firepyp650@gmail.com>
2024-05-07 19:07:28 -0500

    This is probably the 'ssh' version done

--- fsss-ssh.sh
+++ fsss-ssh.sh
@@ -1,8 +1,11 @@
 echo '[INFO] Make sure to run this script as the user that forejo runs as, over ssh, or with manual ownership grants to your current tty!'
+rname="NUL"
+read -p '"Real Name" for gpg and git: ' rname
+email="NUL"
+read -p 'Email: ' email
 echo '[INFO] Now creating a gpg key, please fill out the inforemation with what you want for forejo to use.'
-echo '[INFO] Since the creator is uncertain if it is supported, this script recommends having no passphrase on this key.'
-echo '$ gpg --default-new-key-algo rsa4096 --gen-key'
-gpg --default-new-key-algo rsa4096 --gen-key
+echo "$ gpg --default-new-key-algo rsa4096 --quick-gen-key --batch --passphrase '' '$rname <$email>'"
+gpg --default-new-key-algo rsa4096 --quick-gen-key --batch --passphrase '' '$rname <$email>'
 echo '[INFO] Listing keys'
 echo '$ gpg --list-secret-keys --keyid-format=long'
 gpg --list-secret-keys --keyid-format=long
@@ -11,3 +14,8 @@ echo "[INFO] Detected key: $key_id"
 echo '[INFO] Having git recognize this as the default signing key for this user...'
 echo "$ git config --global user.signingkey $key_id"
 git config --global user.signingkey $key_id
+echo "[INFO] Having git use \"$rname\" as name and \"email\" as email..."
+echo "$ git config --global user.name \"$rname\""
+git config --global user.name "$rname"
+echo "$ git config --global user.email \"$email\""
+git config --global user.email "$email"
