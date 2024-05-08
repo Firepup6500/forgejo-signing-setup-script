ccad60092b89ceec4b3bfce22fe4b4db40c358ee
Firepup Sixfifty <firepyp650@gmail.com>
2024-05-07 19:20:15 -0500

    Add notices

--- fsss-ssh.sh
+++ fsss-ssh.sh
@@ -1,9 +1,10 @@
-echo '[INFO] Make sure to run this script as the user that forejo runs as, over ssh, or with manual ownership grants to your current tty!'
+echo '[NOTICE] Make sure to run this script as the user that forejo runs as, via ssh or with manual ownership grants to your current tty!'
+echo '[NOTICE] This script modifes your default git config! If forejo runs as your user, then this will mess up your default git config!'
 rname="NUL"
 read -p '"Real Name" for gpg and git: ' rname
 email="NUL"
 read -p 'Email: ' email
-echo '[INFO] Now creating a gpg key, please fill out the inforemation with what you want for forejo to use.'
+echo '[INFO] Now creating a gpg key using the real name and email provided'
 echo "$ gpg --default-new-key-algo rsa4096 --quick-gen-key --batch --passphrase '' \"$rname <$email>\""
 gpg --default-new-key-algo rsa4096 --quick-gen-key --batch --passphrase '' "$rname <$email>"
 echo '[INFO] Listing keys'
