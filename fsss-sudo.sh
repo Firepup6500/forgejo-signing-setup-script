148d7b28b37e35a10ed75fc11465c9136de6fdbc
Firepup Sixfifty <firepyp650@gmail.com>
2024-05-09 09:45:47 -0500

    I'm too lazy to support these properly

--- fsss-sudo.sh
+++
@@ -1,8 +0,0 @@
-echo '[INFO] Make sure to run this script as a used with sudo!'
-echo '[INFO] Now creating a gpg key, please fill out the inforemation with what you want for forejo to use.'
-echo '$ gpg --default-new-key-algo rsa4096 --gen-key'
-gpg --default-new-key-algo rsa4096 --gen-key
-echo '[INFO] Listing keys'
-echo '$ gpg --list-secret-keys --keyid-format=long'
-gpg --list-secret-keys --keyid-format=long
-
